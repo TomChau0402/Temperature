9ce56d9188f9599623648fd295b0657f457febe3
TomChau0402 <tung.t.t.chau@gmail.com>
2024-12-12 17:48:37 -0800

    first assignment

---
+++ scripts/script.js
@@ -0,0 +1,50 @@
+console.log("Script");
+
+//string""
+let firstName="chau"
+let lastName="tom"
+//numbers
+let myAge = 59
+let myShoe = 9
+let myHeight= 6 
+let myFun = "running"
+
+//boolean they can be true or flase
+let isStudent = false;
+//display
+console.log(firstName);
+console.log(myAge);
+
+//concatenation
+console.log("my name is" + lastName + "my hieght is" + myHeight + "i like to" + myFun);
+
+let num1 =10;
+let num2=20;
+
+let addition = num1 + num2;
+let sub = num1 - num2;
+let div = num1/num2
+let mult = num1* num2
+
+console.log("addition is: " + addition);
+console.log("substracton is: " + sub);
+console.log("divison is: " + div)
+console.log("multiplication is:" +mult)
+
+let a;
+const pi = 3.14
+let r=prompt("enter te radius")
+a =pi *(r*r);
+console.log("the area of a circle with a radius" + r  + "is the" + a)
+//define the var
+let aVariable;
+
+//define a value
+aVariable=20;
+
+//define and assing the value at teh same time
+let otherVariable = 30
+
+console.log(aVariable,otherVariable)
+const weekDay = 7;
+console.log(weekDay),
---
+++ scripts/scripts/assignment1.js
@@ -0,0 +1,12 @@
+console.log("---------------Story--------")
+
+let nine = "tom"
+let place = "school"
+let time = 8
+let anotherTime = 6
+
+const familyMember = "brother"
+   
+console.log("One day" + nine + "was late for" +  place + "It was pass" + time)
+console.log ("when the stars are schedule to be " + anotherTime)
+console.log(" My" + familyMember + "was there on time, but" +  nine +  "was late to" + place)
